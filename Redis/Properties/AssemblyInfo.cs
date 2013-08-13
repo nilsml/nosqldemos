7795edce3643d10e201c97b57eb8f11e29b01c70
Nils Magne Lunde <nilsml@nilsland.com>
2013-08-13 18:00:16 +0200

    Renamed projects

--- Redis/Properties/AssemblyInfo.cs
+++ Redis/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Redis")]
+[assembly: AssemblyTitle("RedisRepository")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Redis")]
+[assembly: AssemblyProduct("RedisRepository")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
